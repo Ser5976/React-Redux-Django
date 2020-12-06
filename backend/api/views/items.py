b2ebcfd2f60d13ea325b8e92a9a0575375f90cc7
Dmitry Tsmyh <tsmyh@mail.ru>
2020-12-06 13:26:21 +0300

    add swagger

--- backend/api/views/items.py
+++ backend/api/views/items.py
@@ -1,10 +1,11 @@
 # api/views/items.py
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from api.serializers import ItemSerializer
 from todo.models import Item
 
 
 class ItemViewSet(viewsets.ModelViewSet):
+    # permission_classes = [permissions.IsAuthenticated]
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
     'corsheaders',
+
     'todo',
     'users',
 ]
