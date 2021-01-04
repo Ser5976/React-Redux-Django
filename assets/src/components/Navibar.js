2d77dd2db14d69b9526fbcd72f20bf4a6361a6a6
Dmitry Tsmyh <tsmyh@mail.ru>
2021-01-04 10:59:27 +0300

    add dropdown links

--- assets/src/components/Navibar.js
+++ assets/src/components/Navibar.js
@@ -10,25 +10,63 @@ export default function Navibar() {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Django React </Navbar.Brand>
+        <Navbar.Brand>
+          <Link
+            to="/"
+            style={{ textDecoration: 'none', color: '#fff', }}
+          >
+            Django React
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link></Nav.Link>
 
-            <NavDropdown title="Продажа" id="collasible-nav-dropdown">
+            <NavDropdown title="Для частных лиц" id="collasible-nav-dropdown">
               <NavDropdown.Item>
                 <Link
                   to="/ListCard"
                   style={{ textDecoration: 'none', color: '#212529' }}
                 >
-                  Дома
+                  Выбрать дом
                 </Link>
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Замени на что-нибудь полезное
               </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Для бизнеса" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.4">
+                Замени на что-нибудь полезное
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Замени на что-нибудь полезное
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Для администраторов" id="collasible-nav-dropdown">
+                <a
+                  href="http://127.0.0.1:8000/admin/"
+                  style={{ textDecoration: 'none', color: '#212529', 'marginLeft': '20px' }}
+                >
+                  Django admin
+                </a>
+              <NavDropdown.Divider />
+              <a
+                  href="http://127.0.0.1:8000/swagger/"
+                  style={{ textDecoration: 'none', color: '#212529', 'marginLeft': '20px' }}
+              >
+                Swagger
+              </a>
+              <NavDropdown.Divider />
+              <a
+                  href="http://127.0.0.1:8000/silk/"
+                  style={{ textDecoration: 'none', color: '#212529', 'marginLeft': '20px' }}
+              >
+                Silk
+              </a>
             </NavDropdown>
           </Nav>
           <Nav>
