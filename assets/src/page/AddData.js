685e1b48f8ef6163e7124a9503f4c76a54662349
Seriy <tsbaranova@bk.ru>
2021-01-07 20:03:53 +0300

    clearActiveItem

--- assets/src/page/AddData.js
+++ assets/src/page/AddData.js
@@ -15,10 +15,11 @@ const AddData = () => {
     bug,
     image,
   } = useContext(BaseContext);
+  console.log(validated);
   const inputEl = useRef(null);
   const { description, price, address, status, house_type, photo } = activeItem;
   const { country, city, street, house_number, zip_code } = address;
-  console.log(typeof photo);
+
   const radioStatus = [
     { label: 'Продатся', value: 1 },
     { label: 'В продаже', value: 2 },
