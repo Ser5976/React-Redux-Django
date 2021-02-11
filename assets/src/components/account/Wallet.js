4a4ee6eb08694de26d93182dd9bea0d1320c7737
Seriy <tsbaranova@bk.ru>
2021-02-11 22:02:44 +0300

    Accordion

--- assets/src/components/account/Wallet.js
+++ assets/src/components/account/Wallet.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Accordion, Button } from 'react-bootstrap';
 
 const Wallet = ({ wallet }) => {
+  //console.log(wallet);
   const wallets = wallet.map((money) => {
     return (
-      <Card>
-        <Card.Header as="h5" className="text-center">
-          {money.currency.name}
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>
-            <Row className="justify-content-between">
-              <Col sm={4}>Ваш баланc:</Col>
-              <Col sm={5}>
-                {money.balance} <span></span>
-                {money.currency.symbol}
-              </Col>
-            </Row>
-          </Card.Title>
-          <Card.Text></Card.Text>
-        </Card.Body>
-      </Card>
+      <Accordion key={money.id}>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle
+              as={Button}
+              variant="link"
+              eventKey={money.id}
+              className="text-center"
+            >
+              {money.currency.name}
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={money.id}>
+            <Card.Body>
+              {' '}
+              <Row className="justify-content-between">
+                <Col sm={4}>Ваш баланc:</Col>
+                <Col sm={5}>
+                  {money.balance} <span></span>
+                  {money.currency.symbol}
+                </Col>
+              </Row>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     );
   });
   return <>{wallets}</>;
