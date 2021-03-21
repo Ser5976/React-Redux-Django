b9c38aba4a2a615b0a0d4a6a094223284a30df55
Tsmyh, Dzmitry <dtsmyh@scnsoft.com>
2021-03-21 18:50:17 +0300

    add is_admin

--- backend/backend/auth.py
+++ backend/backend/auth.py
@@ -6,7 +6,8 @@ class CustomLoginView(LoginView):
     def get_response(self):
         original_response = super().get_response()
         data = {"user_id": self.user.id, "username": self.user.username,
-                'role': self.user.role, "status": "success"}
+                'role': self.user.role, "status": "success",
+                'is_admin': self.user.is_admin}
         original_response.data.update(data)
         # add cookie to response
         # token = original_response.data.get('key')
@@ -23,7 +24,8 @@ class CustomRegisterView(RegisterView):
     def get_response_data(self, user):
         original_response = super().get_response_data(user)
         data = {"user_id": user.id, "username": user.username,
-                'role': user.role, "status": "success"}
+                'role': user.role, "status": "success",
+                'is_admin': user.is_admin}
         original_response.update(data)
         # add cookie to response
         # token = original_response.data.get('key')
