becb6ebdb0bd6744e5d2fed85ed85db1dee18bfb
Dmitry Tsmyh <tsmyh@mail.ru>
2021-06-10 21:47:57 +0300

    add item filtering by auth

--- backend/api/views/items.py
+++ backend/api/views/items.py
@@ -10,7 +10,19 @@ from todo.models import Item, Comment, Address
 class ItemViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.IsAuthenticated]
     serializer_class = ItemSerializer
-    queryset = Item.objects.order_by('-created_at')
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        queryset = Item.objects.order_by('-created_at')
+        filter_param = self.request.query_params.get('filter')
+        if filter_param is not None and filter_param == 'auth':
+            queryset = queryset.filter(owner=user)
+
+        return queryset
 
     # def create(self, request, *args, **kwargs):
     #     serializer = self.get_serializer(data=request.data)
